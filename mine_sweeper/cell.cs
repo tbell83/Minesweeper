2c9d5a0069447f0a1eb439d38ed1d006421e13be
tbell83 <tbell@tombellnj.com>
2014-04-05 11:48:45 -0400

    commit

---
+++ mine_sweeper/cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;                                                                                                                                                                                          
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mine_sweeper{
+    class cell{
+        bool mine = false;
+        bool flagged = false;
+        bool covered = true;
+        int neighbors;
+
+        public cell(){}
+
+        public void setNeighbors(int neighbors){
+            this.neighbors = neighbors;
+        }
+
+        public void setMined(bool mine){
+            this.mine = mine;
+        }
+
+        public void setFlagged(bool flagged){
+            this.flagged = flagged;
+        }
+
+    }
+}
---
+++ mine_sweeper/minesweeper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mine_sweeper{
+    class minesweeper{
+        int size;
+        int mines;
+        List<cell> grid = new List<cell>();
+
+        public minesweeper(int size, int mines){
+            size = this.size;
+            mines = this.mines;
+            for (int i = 0; i < Math.Pow(size,2)){
+                cell cell = new cell();
+            }
+        }
+
+        private void mineTheField(){
+            for(int i = 0;i < mines; i++){
+                
+            }
+
+    }
+}
